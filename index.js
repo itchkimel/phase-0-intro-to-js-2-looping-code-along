16023b1a0c4f55672b32c318afbeb9b8fd26d703
Yitzchok Kimelman <itchkimel@gmail.com>
2021-03-10 20:50:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function giftWrap(jgifts){
+  for (let i = 0; i < jgifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+  return gifts;
+}
+giftWrap(gifts);
+*/
+
+function writeCards(personalNames, supriseEvent) {
+  const array = [];
+  for (let i = 0; i < personalNames.length; i++){
+    array.push(`Thank you, ${personalNames[i]}, for the wonderful ${supriseEvent} gift!`);
+  };
+  return array;
+};
+
+
+function countDown(n){
+  while (n >= 0){
+    console.log(n);
+    n--;
+  }
+}
